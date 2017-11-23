12d618672fdf850d993811304875b011c14dfb4c
Anton Strömkvist <anton@stromkvist.com>
2017-11-23 12:12:35 +0100

    Add simple file to test with node

---
+++ test.js
@@ -0,0 +1,8 @@
+const EventStoreClient = require("./index.js");
+
+const EVENT_STORE_WEBSOCKET_URL = "ws://event-store.5z.fyi/realtime";
+const TEST_ID = "AAABXsUq9uIKAAMzAAAAAA";
+
+const client = new EventStoreClient(EVENT_STORE_WEBSOCKET_URL);
+client.subscribe("sms", TEST_ID, x => console.log(x.length));
+client.rewind("sms", [TEST_ID], false, 100);
